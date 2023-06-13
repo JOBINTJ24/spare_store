404c850e45014b55ad46d7f68927709d16b518b7
JOBIN T J <98311514+JOBINTJ24@users.noreply.github.com>
2023-06-13 15:22:55 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        ) from ex
     execute_from_command_line(sys.argv)
 
 
