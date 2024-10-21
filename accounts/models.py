c7cdbb3c7f8ab82dcd5cd4520a3ec433d090e381
JOBIN T J <98311514+JOBINTJ24@users.noreply.github.com>
2024-10-21 22:39:09 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -46,7 +46,7 @@ class Account(AbstractBaseUser):
 
     objects = MyAccountManager()
 
-    # def full_name(self):
+  
     #     return f'{self.first_name} {self.last_name}'
 
     def _str_(self):
